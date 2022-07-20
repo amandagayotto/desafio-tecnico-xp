b71f3478f57d1c70d584da12138f667a1bc0f5c6
Amanda <amandagayotto@hotmail.com>
2022-07-19 22:15:52 -0300

    build: inicializando sequelize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const app = require('./app');
+require('dotenv').config();
 
-const port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
+
+const port = process.env.API_PORT;
 app.listen(port, () => console.log('ouvindo porta', port));
+
+module.exports = app;
