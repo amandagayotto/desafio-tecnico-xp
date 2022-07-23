8dfa363a1dfaef636d5a80adbe4698bba32235b1
Amanda <amandagayotto@hotmail.com>
2022-07-22 22:22:27 -0300

    feat: cria middleware 'errorHandler'

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('express-async-errors');
+
 const assetController = require('./controllers/assetController');
 const accountController = require('./controllers/accountController');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/assets', assetController);
 app.use('/account', accountController);
+app.use(errorHandler);
 
-const port = process.env.API_PORT;
+const port = 3000;
 app.listen(port, () => console.log('ouvindo porta', port));
 
 module.exports = app;
